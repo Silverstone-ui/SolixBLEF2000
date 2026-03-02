08b8abbf91099f5abf6993bcccd08d50cdb6538c
Harvey Lelliott <42912136+flip-dots@users.noreply.github.com>
2026-03-02 21:28:25 +0000

    Attempt to fix issue with autogenerated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../SolixBLE"))
 
 project = "SolixBLE"
 copyright = "2026, Harvey Lelliott"
