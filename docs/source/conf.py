44a6cb57cbedb6f7cfa7c370749d669b62e23feb
Harvey Lelliott <42912136+flip-dots@users.noreply.github.com>
2026-03-11 16:22:03 +0000

    Fix broken docs auto generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../SolixBLE"))
+sys.path.insert(0, os.path.abspath("../../"))
 
 project = "SolixBLE"
 copyright = "2026, Harvey Lelliott"
